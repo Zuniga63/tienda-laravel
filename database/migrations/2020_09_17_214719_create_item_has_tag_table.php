4328349c659bbfa5ae4c3f09b2a26dc3d49436ad
Andres Zuñiga <andres.zuniga.063@gmail.com>
2020-09-17 16:48:15 -0500

    the item has tag table migration was created

---
+++ database/migrations/2020_09_17_214719_create_item_has_tag_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemHasTagTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('item_has_tag', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('item_id')->constrained('item')->onDelete('cascade');
+            $table->foreignId('tag_id')->constrained('tag')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('item_has_tag');
+    }
+}
