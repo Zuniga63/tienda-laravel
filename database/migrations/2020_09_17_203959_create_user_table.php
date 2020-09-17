d9b49a80f55830cebc6daba2b77207c0a9c73749
Andres Zuñiga <andres.zuniga.063@gmail.com>
2020-09-17 15:43:38 -0500

    the user table migration was created

---
+++ database/migrations/2020_09_17_203959_create_user_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->id();
+            $table->string('username', 50)->unique();
+            $table->string('password', 100);
+            $table->string('first_name', 50);
+            $table->string('last_name', 50)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user');
+    }
+}
