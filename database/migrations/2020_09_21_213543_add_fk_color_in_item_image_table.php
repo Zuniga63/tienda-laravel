ddcb47f0cdc882599280b224bad0ab1247d4f243
Andres Zuñiga <andres.zuniga.063@gmail.com>
2020-09-28 08:56:58 -0500

    add function dropForeig

--- database/migrations/2020_09_21_213543_add_fk_color_in_item_image_table.php
+++ database/migrations/2020_09_21_213543_add_fk_color_in_item_image_table.php
@@ -6,27 +6,28 @@ use Illuminate\Support\Facades\Schema;
 
 class AddFkColorInItemImageTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::table('item_image', function (Blueprint $table) {
-            $table->foreignId('color_id')->nullable()->after('item_id')->constrained('color')->onDelete('set null');
-        });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::table('item_image', function (Blueprint $table) {
+      $table->foreignId('color_id')->nullable()->after('item_id')->constrained('color')->onDelete('set null');
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::table('item_image', function (Blueprint $table) {
-            $table->dropColumn('color_id');
-        });
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::table('item_image', function (Blueprint $table) {
+      $table->dropForeign('item_image_color_id_foreign');
+      $table->dropColumn('color_id');
+    });
+  }
 }
